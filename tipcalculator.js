7476dc1522529dc81b81b82c46f8e75d03891582
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-15 18:13:03 +0200

    validate input

--- tipcalculator.js
+++ tipcalculator.js
@@ -4,3 +4,19 @@ function calculateTip() {
   var billAmt = document.getElementById("billamt").value;
   var serviceQual = document.getElementById("serviceQual").value;
   var numOfPeople = document.getElementById("peopleamt").value;
+
+    //validate input
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+  }
+  //Check to see if this input is empty or less than or equal to 1
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //Calculate tip
+  var total = (billAmt * serviceQual) / numOfPeople;
