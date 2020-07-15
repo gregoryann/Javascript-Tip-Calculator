a46c33f464336395e8326da90c769718d51ed149
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-15 18:14:10 +0200

    round to two decimal places

--- tipcalculator.js
+++ tipcalculator.js
@@ -20,3 +20,15 @@ function calculateTip() {
 
   //Calculate tip
   var total = (billAmt * serviceQual) / numOfPeople;
+  
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+}
+
+
