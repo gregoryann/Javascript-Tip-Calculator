257af36e747e3c86865318a2abe4be9c71c4e6af
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-15 18:12:19 +0200

    Calculate Tip

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,1 +1,6 @@
 
+//Calculate Tip
+function calculateTip() {
+  var billAmt = document.getElementById("billamt").value;
+  var serviceQual = document.getElementById("serviceQual").value;
+  var numOfPeople = document.getElementById("peopleamt").value;
